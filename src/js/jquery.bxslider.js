0e12a0453b86c91b346091ddf0e3b00d567e833f
Matthew Gallantry <gallantrym@infrastructures-MacBook-Pro-2.local>
2016-02-25 15:06:23 +0000

    Fixed issue with clicking the slider multiple times

--- src/js/jquery.bxslider.js
+++ src/js/jquery.bxslider.js
@@ -1331,15 +1331,18 @@
       position = {left: 0, top: 0},
       lastChild = null,
       lastShowingIndex, eq, value, requestEl;
+
+
+      var potentialSlide = setSlideIndex(slideIndex);
+      // if plugin is currently in motion, ignore request
+      if (slider.working || slider.active.index === potentialSlide) { return; }
+      // declare that plugin is in motion
+      slider.working = true;
+
       // store the old index
       slider.oldIndex = slider.active.index;
       //set new index
       slider.active.index = setSlideIndex(slideIndex);
-
-      // if plugin is currently in motion, ignore request
-      if (slider.working || slider.active.index === slider.oldIndex) { return; }
-      // declare that plugin is in motion
-      slider.working = true;
 
       performTransition = slider.settings.onSlideBefore.call(el, slider.children.eq(slider.active.index), slider.oldIndex, slider.active.index);
 
